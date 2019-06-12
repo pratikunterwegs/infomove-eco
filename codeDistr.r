e31f40326128a50ae038cd7958f0f5633e4bad79
pratik <pratikgupte16@gmail.com>
2019-06-12 08:57:22 +0200

    vis green wave and up gitig

--- codeDistr.r
+++ codeDistr.r
@@ -15,6 +15,8 @@ land <- read_csv("migSimCode/testLandOutput.csv", col_names = T)
 land <- mutate(land, time = 1:nrow(land)) %>% 
   gather(site, resources, -time)
 
-ggplot(land)+
+ggplot(land %>% filter(time %% 20 == 0))+
   geom_tile(aes(x = as.numeric(site), y = time, fill = resources))+
-  scale_y_reverse()
+  scale_y_reverse()+
+  scale_fill_viridis_c(limits = c(0,1), option = "plasma")+
+  theme_void()
