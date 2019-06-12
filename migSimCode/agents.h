6152db95d5163c036c1fe0dcf0dfe8e6fadae968
pratik <pratikgupte16@gmail.com>
2019-06-12 09:10:29 +0200

    func clear node state
    add age func

--- migSimCode/agents.h
+++ migSimCode/agents.h
@@ -27,6 +27,19 @@ using Ann = Network<float,
 	Layer< Neuron<3, activation::rtlu >, 1>
 >;
 
+// clear node state
+struct flush_rec_nodes
+{
+	template <typename Neuron, typename T>
+	void operator()(T* state, size_t /*layer*/, size_t /*neuron*/)
+	{
+		auto it = state + Neuron::feedback_scratch_begin;
+		for (auto i = 0; i < Neuron::feedback_scratch; ++i) {
+			it[i] = T(0);
+		}
+	}
+};
+
 // dec agent class
 class agent
 {
@@ -41,19 +54,22 @@ class agent
 		void doChoice();
 		void doGetFitness();
 		void doAddComp();
+		void doAge();
 		void reproduce();
 };
 
 // def choice func
+// agents have 2 inputs and 1 output
 void agent::doChoice()
 {
-	float in1 = landscape[position].resource;
-	float in2 = landscape[position].propAgentsMigrating;
+	float in1 = landscape[position].resource;  // input 1 is resource level
+	float in2 = landscape[position].propAgentsMigrating;  // input 2 is nmber of other agents moving forward
 
 	// agents assess resources and proportion of flock moving forward
 	Ann::input_t inputs; inputs[0] = in1; inputs[1] = in2;
 	auto output = brain(inputs);
 
+	// if output > 0 agents move forward and add to the signal
 	if (output[0] > 0) { 
 		keepGoing = true; 
 		landscape[position].nAgentsMigrating += 1;
@@ -61,14 +77,23 @@ void agent::doChoice()
 	}
 }
 
+// agents add competitive pressure proportional to age
+// this can be increased in mechanistic detail
 void agent::doAddComp()
 {
 	landscape[position].totalComp += age;
 }
 
+// agents extract resources scaled by their age-based competitiveness on the site
 void agent::doGetFitness()
 {
 	fitness += landscape[position].resource * (static_cast<float> (age) / static_cast<float> (landscape[position].totalComp));
 }
 
-// 
+// function agents age, maybe be useful later
+void agent::doAge()
+{
+	age++;
+}
+
+// ends here
