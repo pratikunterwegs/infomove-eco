7500ea42e8ba10eee07caf4215b388a0d1938da0
pratik <pratikgupte16@gmail.com>
2019-08-11 13:08:47 +0200

    simplify landscape to continuous

--- migSimCode/landscape.h
+++ migSimCode/landscape.h
@@ -12,44 +12,25 @@
 #include <string>
 
 // size parameters
-const int landsize = 1000; // keep even
-const int popsize = 100; // not too many
-const int tMax = 200; // must always be less than landsize
+// const int landsize = 1000; // not necessary
+const int popsize = 1000; // not too many
+const int tMax = 200; // around 200 days
 
 // green wave params
-const int initpeak = 10; // arbitrary choice greater than 0
-int currentpeak = initpeak; // to begin
-const int finalpeak = landsize - initpeak + 100;
-const int waveVelocity = 5; // how fast the peak moves
+const float initpeak = 10.f; // arbitrary choice greater than 0
+float currentpeak = initpeak; // to begin
+// const int finalpeak = landsize - initpeak + 100;
+
+// wave velocity is a float drawn from a normal dist
+// normal dist
+std::mt19937_64 rng;
+std::normal_distribution<float> normDist(1.f, 0.5f);
+
+const float waveVelocity = normDist(rng); // how fast the peak moves
 const float peakvalue = 100.f;
-const float steepness = 0.01f;
+const float steepness = 0.05f;
 
-// season params
-const int nSeasons = 100; //arbit choice
+// seasons or replicates params
+const int nSeasons = 2000; //arbit choice
 
-// init landscape struct
-class site {
-public:
-	float resource;
-	int nAgents;
-	int nAgentsMigrating;
-	int totalComp;
-	float propAgentsMigrating;
-
-};
-
-// init vector of sites as landscape
-std::vector<site> landscape(landsize);
-
-
-// flip landscape
-void flipLand(std::vector<site> &landscape)
-{
-	// flip the values
-	std::rotate(landscape.begin(), landscape.end() - 1, landscape.end());
-	
-	// flip peak location
-	currentpeak = initpeak;
-}
-
-//
+// end here
