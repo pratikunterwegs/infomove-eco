0b96ad3a8e9e91db6093023488e0d203523c0ccb
pratikunterwegs <pratikgupte16@gmail.com>
2019-08-13 16:47:46 +0200

    plot neighbours over time

--- codeRes.r
+++ codeRes.r
@@ -20,8 +20,7 @@ ggplot(data[time %% 20 == 0 & gen %% 50 ==0,])+
                  position = "identity", bins = 50, alpha = 0.7)+
   scale_fill_viridis_c()+
   geom_vline(xintercept = 1, col=4)+
-  facet_grid(gen~time, scales = "fixed", labeller = label_both)+
-  coord_cartesian(xlim = c(-1e2, 1e2))
+  facet_grid(gen~time, scales = "fixed", labeller = label_both)
   # labs(title = "wave velocity = 5.0, gens = 500")
 
 
@@ -30,9 +29,11 @@ d = fread("migSimCode/agentpos.csv")
 
 # dist over time
 ggplot(d[id %% 10 == 0,])+
-  geom_point(aes(time, pos), size = 0.1, alpha = 0.1)+
-  geom_line(aes(time, peakpos), col = 2, size = 0.1)+
-  facet_grid(~gen)
+  geom_line(aes(time, pos - peakpos, group = id), size = 0.1, col = 4)+
+  geom_hline(yintercept = 0, col = 2)+
+  # geom_line(aes(time, peakpos), col = 2, size = 0.1)+
+  facet_grid(~gen)+
+  coord_cartesian(ylim = c(-1e2, 100))
 
 # energy over time
 ggplot(d)+
@@ -46,3 +47,9 @@ ggplot(d[gen > 150,])+
   geom_point(aes(time, dist), size = 0.1, alpha = 0.1)+
   # geom_line(aes(time, peakpos), col = 2, size = 0.1)+
   facet_grid(~gen)
+
+# neighbours over time
+ggplot(d)+
+  geom_path(aes(time, neighbours, group = id), size = 0.1)+
+  #coord_cartesian(ylim=c(0,1e2))+
+  facet_wrap(~gen, scales = "free")
