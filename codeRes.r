c52963f66b41521e71e9a3c4d73d88f525151edd
pratikunterwegs <pratikgupte16@gmail.com>
2019-08-13 13:43:55 +0200

    minor change to plotting

--- codeRes.r
+++ codeRes.r
@@ -1,36 +1,27 @@
 #### code to look at cpp output ####
 
 library(data.table)
+library(ggplot2)
+library(ggthemes)
+x11()
 
-rm(data)
-gc()
+rm(data);gc()
 
 {data <- fread("migSimCode/testAgentPos.csv")
 data <- data[,lapply(.SD, as.numeric), .SDcols = names(data)]
 data <- na.omit(data)
 head(data)}
 
-# data[,nsd:=c(NA, diff(pos)),
-#      by=list(id, gen)]
-
-# plot data
-library(ggplot2)
- x11()
-
-randagent <- round(runif(200, 0, 99))
-
 # histogram of peak distance at each timestep
-setDT(data)
-ggplot(data[gen %% 30 == 0,])+
+#setDT(data)
+ggplot(data[time %% 30 == 0,])+
   geom_histogram(aes(x = distpeak, group = gen),
                  fill = 2,
                  position = "identity", bins = 50, alpha = 0.7)+
   scale_fill_viridis_c()+
-  geom_vline(xintercept = 0)+
-  # scale_fill_
+  geom_vline(xintercept = 0, col=4)+
   facet_grid(gen~time, scales = "fixed", labeller = label_both)+
-  theme_void()
-  # xlim(-100, 100)+
+  theme_few()
   # labs(title = "wave velocity = 5.0, gens = 500")
 
 ggsave("figTest01.png", dpi = 300, 
