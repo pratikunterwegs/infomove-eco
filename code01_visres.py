1dbf7e858e991d55fd712c8e3bc227584577ae68
pratikunterwegs <pratikgupte16@gmail.com>
2019-08-16 14:27:44 +0200

    add hist in python

--- code01_visres.py
+++ code01_visres.py
@@ -1,1 +1,24 @@
-# this is some prelim python data
+# this is some prelim python code
+
+# check python path
+import sys
+
+# should yield python 3.7 file path
+for p in sys.path:
+    print(p)
+
+import pandas as pd # similar to dplyr! yay!
+import os  # has list dir functions etc
+import numpy as np  # some matrix functions
+from scipy import misc
+import matplotlib.pyplot as plt
+
+# check the current working directory
+os.getcwd()
+currentWd = p # os.path.dirname(os.path.abspath(__file__)) #os.getcwd()
+
+# check again
+print(currentWd)
+
+# read in data
+d = pd.read_csv("migSimCode/agentpos.csv")
