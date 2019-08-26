85c72484bc22c1f5b2f13997fdf80b1ccbb827dc
pratikunterwegs <pratikgupte16@gmail.com>
2019-08-26 14:37:18 +0200

    remove unn libs

--- migSimCode/agents.h
+++ migSimCode/agents.h
@@ -3,17 +3,7 @@
 // code to init agents
 
 #include <iostream>
-
 #include <vector>
-#include <random>
-#include <cstdlib>
-#include <iostream>
-#include <cmath>
-#include <cassert>
-#include <random>
-#include <chrono>
-#include <string>
-#include <utility>
 #include <algorithm>
 #include <functional>
 #include <numeric>
