a3b057d61a4f35625e620bbc0791abc28fc4a725
pratikunterwegs <pratikgupte16@gmail.com>
2019-08-26 14:21:23 +0200

    move working tests to main

--- migSimCode/testAgents.cpp
+++ migSimCode/testAgents.cpp
@@ -1,59 +1,55 @@
 /// code to run tests on the simulation
 
-// include some headers
-#include <cassert>
-#include "agents.h" // may complain if lacking ann.h
-
 // list of tests
 /// test that the vector of agents has at least one agent
 void test_agent_vec()
 {
     // check size of vector
-    assert((init_agents(10)).size() == 10);
+    assert((initAgents(10)).size() == 10);
 }
 
-/// test that the distance matrix is correctly created
-void test_make_dmatrix()
-{
-    // make some agents
-    std::vector<agent> testpop = initAgents(10);
-    // check that matrix of 10 agents has 10 * 10 cells
-    std::vector<std::vector<float> > test_dmatrix = make_distmatrix(testpop);
-    assert( test_dmatrix.size() * test_dmatrix[0].size() == 100) );
-
-    // test diagonal is zero
-    for (int iter = 0; iter < testpop.size(); iter++)
-    {
-        assert(test_dmatrix[iter][iter] == 0.f);
-    }
-    
-}
-
-/// test the distance matrix is correct updated
-{
-    // create a population
-
-    // make distance matrix
-
-    // move the population manually
-
-    // update the matrix
-
-    // check diagonal remains zero
-
-    // check updated values are correct
-}
-
-
-/// test that agents sense the correct number of neighbours
-void test_sense_neighbours()
-{
-    
-
-}
-
-/// test that agents share food with correct number of neighbours
-
-
-
-/// test that the distance matrix is correctly updated
+///// test that the distance matrix is correctly created
+//void test_make_dmatrix()
+//{
+//    // make some agents
+//    std::vector<agent> testpop = initAgents(10);
+//    // check that matrix of 10 agents has 10 * 10 cells
+//    std::vector<std::vector<float> > test_dmatrix = make_distmatrix(testpop);
+//    assert( test_dmatrix.size() * test_dmatrix[0].size() == 100) );
+//
+//    // test diagonal is zero
+//    for (int iter = 0; iter < testpop.size(); iter++)
+//    {
+//        assert(test_dmatrix[iter][iter] == 0.f);
+//    }
+//    
+//}
+//
+///// test the distance matrix is correct updated
+//{
+//    // create a population
+//
+//    // make distance matrix
+//
+//    // move the population manually
+//
+//    // update the matrix
+//
+//    // check diagonal remains zero
+//
+//    // check updated values are correct
+//}
+//
+//
+///// test that agents sense the correct number of neighbours
+//void test_sense_neighbours()
+//{
+//    
+//
+//}
+//
+///// test that agents share food with correct number of neighbours
+//
+//
+//
+///// test that the distance matrix is correctly updated
