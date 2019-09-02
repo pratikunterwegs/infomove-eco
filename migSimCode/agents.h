dc384f9c2da2848c54cd35fbef2a9eba4af44cab
pratikunterwegs <pratikgupte16@gmail.com>
2019-09-02 17:41:32 +0200

    get float rand vals

--- migSimCode/agents.h
+++ migSimCode/agents.h
@@ -28,7 +28,7 @@ using Ann = Network<float,
 >;
 
 // pick rand node weights
-std::uniform_real_distribution<double> dist(-1.f, 1.f);
+std::uniform_real_distribution<float> dist(-1.f, 1.f);
 
 // pick rand position
 // std::uniform_real_distribution<double> pos(0.0, 200.0);
