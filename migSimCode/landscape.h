3f0238878ae9252da1aefa450d41f0da35fa8774
pratikunterwegs <pratikgupte16@gmail.com>
2019-09-30 14:13:07 +0200

    add vec output code to land header

--- migSimCode/landscape.h
+++ migSimCode/landscape.h
@@ -9,14 +9,23 @@
 #include <chrono>
 #include <string>
 #include <fstream>
+#include <ostream>
+
+/// function to print vector output
+template <class T>
+void out_vec(const std::vector<T>& vec)
+{
+	std::copy(std::begin(vec), std::end(vec), std::ostream_iterator<T>(std::cout, " "));
+	std::cout << "\n";
+}
 
 // size parameters
 // const int landsize = 1000; // not necessary
-const int popsize = 500; // not too many
-const int tMax = 100; // not too high
+const int popsize = 50; // not too many
+const int tMax = 10; // not too high
 
 // green wave params
-const float initpeak = 10.f; // arbitrary choice greater than 0
+const float initpeak = 100.f; // arbitrary choice greater than 0
 float currentpeak = initpeak; // to begin
 // const int finalpeak = landsize - initpeak + 100;
 
