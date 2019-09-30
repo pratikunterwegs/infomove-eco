5f9ee7cb5955f5aed5ce48cd2dfb1b50090f588d
pratikunterwegs <pratikgupte16@gmail.com>
2019-09-30 14:14:23 +0200

    remove excess functions

--- migSimCode/testPointers.cpp
+++ migSimCode/testPointers.cpp
@@ -23,40 +23,6 @@ void cout_vector(const std::vector<T>& vec)
   std::copy(std::begin(vec), std::end(vec), std::ostream_iterator<T>(std::cout, " "));
 }
 
-/// functions to remove duplicates without sorting
-struct target_less
-{
-    template<class It>
-    bool operator()(It const &a, It const &b) const { return *a < *b; }
-};
-struct target_equal
-{
-    template<class It>
-    bool operator()(It const &a, It const &b) const { return *a == *b; }
-};
-
-template<class It> It uniquify(It begin, It const end)
-{
-    std::vector<It> v;
-    v.reserve(static_cast<size_t>(std::distance(begin, end)));
-    for (It i = begin; i != end; ++i)
-    { v.push_back(i); }
-    std::sort(v.begin(), v.end(), target_less());
-    v.erase(std::unique(v.begin(), v.end(), target_equal()), v.end());
-    std::sort(v.begin(), v.end());
-    size_t j = 0;
-    for (It i = begin; i != end && j != v.size(); ++i)
-    {
-        if (i == v[j])
-        {
-            using std::iter_swap; iter_swap(i, begin);
-            ++j;
-            ++begin;
-        }
-    }
-    return begin;
-}
-
 
 /// create class of agent with single attribute
 class agent{
