13ff496b81ac12230f436066d13f8a0948547c06
pratikunterwegs <pratikgupte16@gmail.com>
2019-09-30 19:01:26 +0200

    output move boolean

--- migSimCode/agents.h
+++ migSimCode/agents.h
@@ -295,7 +295,7 @@ void printData(const int& gen_p, const int& time_p)
 	agentofs.open("dataOut.csv", std::ofstream::out | std::ofstream::app);
 
 	// col header
-	if (gen_p == 0 && time_p == 0) { agentofs << "gen,time,id,movep,movepcopy,chainlength,leader,energy,peakpos\n"; }
+	if (gen_p == 0 && time_p == 0) { agentofs << "gen,time,id,movep,movepcopy,chainlength,leader,energy,bmove\n"; }
 
 	// print for each ind
 	/*if ((gen_p == 0 || gen_p % 1 == 0) && time_p % 20 == 0) */
