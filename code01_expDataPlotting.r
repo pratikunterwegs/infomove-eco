4121ee0352fbe8d6c78a8ac16478a2c52c944837
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-23 16:11:41 +0200

    r code update

--- code01_expDataPlotting.r
+++ code01_expDataPlotting.r
@@ -1,5 +1,7 @@
 #### code to analyse data ####
 
+rm(list = ls())
+
 # load libs
 library(data.table)
 library(ggplot2)
@@ -9,7 +11,10 @@ library(ggthemes)
 ci = function(x){qnorm(0.975)*sd(x, na.rm = T)/sqrt(length(x))}
 
 # load datas
-data = fread("migSimCode/dataOut.csv")
+data = fread("migSimCode/dataOutExp.csv")
+
+# make numeric
+data = data[,lapply(.SD, as.numeric), .SDcols = c(names(data))]
 
 dataSub = data[gen %% 5 == 0,]
 
@@ -18,8 +23,7 @@ dataSub = data[gen %% 5 == 0,]
 {
   dataSum = dataSub[,.(gen,id,movep,movepcopy)]
   dataSum = melt(dataSum, id.vars = c("gen","id"))
-  dataSum = dataSum[,`:=`(peakdiff = abs(100 - value),
-                        roundval = plyr::round_any(value, 5))
+  dataSum = dataSum[,`:=`(roundval = plyr::round_any(value, 100))
                   ][,.N, by = c("gen", "roundval","variable")]
 }
 
@@ -32,11 +36,11 @@ ggplot(dataSum)+
   facet_wrap(~variable, labeller = labeller(variable = labels))+
   scale_fill_viridis_c(option = "B", direction = -1)+
   theme_gdocs()+
-  coord_cartesian(ylim=c(-10,150))+
+  ylim(0, 2000)+
   labs(x = "generation", y = "movement distance",
-       title = "peak = 50/100, N agents = 500, genMax = 200, tMax = 10",
+       title = "depletable land, N agents = 500, genMax = 200, tMax = 10",
        subtitle = "init move = 0.0 : 100.0, init follow = -1.0 : 1.0")
 
 # export plot
-ggsave(filename = "figs/figAltPeak.png", device = png(), width = 10, 
+ggsave(filename = "figs/figExploit.png", device = png(), width = 10, 
        height = 8, dpi = 300); dev.off()
