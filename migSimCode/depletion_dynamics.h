57035502ae07c78ebc5f7d4dac5a0e0c8ca730cd
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-23 16:24:34 +0200

    limit land extension

--- migSimCode/depletion_dynamics.h
+++ migSimCode/depletion_dynamics.h
@@ -31,23 +31,29 @@ void extendLandscape()
 {
 	float movemax = 1.f;
 	// get max intrinsic move param of population
-	for (int p = 0; p < popsize; p++) {
+	for (int p = 0; p < popsize; p++) 
+	{
 		movemax = (movemax > population[p].moveDist) ? movemax : population[p].moveDist;
 	}
 
-	// compare movemax with landscape length and expand
-	if (static_cast<int>(floor(movemax)) >= landscape.size())
+	// how much to extend
+	int landExtend = static_cast<int>(floor(movemax)) >= landscape.size();
+
+	// extend landscape up to the max land size
+	if (landscape.size() + landExtend <= 1000) 
 	{
-		// print extension
-		// make vector of extra grid cells
-		std::vector<gridcell> extraland(static_cast<int>(floor(movemax)) - landscape.size() + 1); // adding one to handle errors
+		// compare movemax with landscape length and expand
+		if (static_cast<int>(floor(movemax)) >= landscape.size())
+		{
+			// print extension
+			// make vector of extra grid cells
+			std::vector<gridcell> extraland(static_cast<int>(floor(movemax)) - landscape.size() + 1); // adding one to handle errors
 
-		landscape.insert(landscape.end(), extraland.begin(), extraland.end());
-		// print success extend
-		std::cout << "land extended to " << landscape.size() << "\n";
+			landscape.insert(landscape.end(), extraland.begin(), extraland.end());
+			// print success extend
+			std::cout << "land extended to " << landscape.size() << "\n";
+		}
 	}
-	
-
 }
 
 /// functions of landscape dynamics
