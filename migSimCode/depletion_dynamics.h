2e40f6d974f5c9bf44ba712d01a7474abb7cc735
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-24 09:40:25 +0200

    set landmax and no expansion

--- migSimCode/depletion_dynamics.h
+++ migSimCode/depletion_dynamics.h
@@ -22,39 +22,39 @@ public:
 
 };
 
-// init landscape of length 100
-std::vector<gridcell> landscape(100);
+// init landscape of length maxland 1000
+std::vector<gridcell> landscape(maxLand);
 
 /// functions of landscape size
 // func to extend landscape
-void extendLandscape()
-{
-	float movemax = 1.f;
-	// get max intrinsic move param of population
-	for (int p = 0; p < popsize; p++) 
-	{
-		movemax = (movemax > population[p].moveDist) ? movemax : population[p].moveDist;
-	}
-
-	// how much to extend
-	int landExtend = static_cast<int>(floor(movemax)) >= landscape.size();
-
-	// extend landscape up to the max land size
-	if (landscape.size() + landExtend <= 1000) 
-	{
-		// compare movemax with landscape length and expand
-		if (static_cast<int>(floor(movemax)) >= landscape.size())
-		{
-			// print extension
-			// make vector of extra grid cells
-			std::vector<gridcell> extraland(static_cast<int>(floor(movemax)) - landscape.size() + 1); // adding one to handle errors
-
-			landscape.insert(landscape.end(), extraland.begin(), extraland.end());
-			// print success extend
-			std::cout << "land extended to " << landscape.size() << "\n";
-		}
-	}
-}
+//void extendLandscape()
+//{
+//	float movemax = 1.f;
+//	// get max intrinsic move param of population
+//	for (int p = 0; p < popsize; p++) 
+//	{
+//		movemax = (movemax > population[p].moveDist) ? movemax : population[p].moveDist;
+//	}
+//
+//	// how much to extend
+//	int landExtend = static_cast<int>(floor(movemax)) >= landscape.size();
+//
+//	// extend landscape up to the max land size
+//	if (landscape.size() + landExtend <= 1000) 
+//	{
+//		// compare movemax with landscape length and expand
+//		if (static_cast<int>(floor(movemax)) >= landscape.size())
+//		{
+//			// print extension
+//			// make vector of extra grid cells
+//			std::vector<gridcell> extraland(static_cast<int>(floor(movemax)) - landscape.size() + 1); // adding one to handle errors
+//
+//			landscape.insert(landscape.end(), extraland.begin(), extraland.end());
+//			// print success extend
+//			std::cout << "land extended to " << landscape.size() << "\n";
+//		}
+//	}
+//}
 
 /// functions of landscape dynamics
 // function to generate food
@@ -92,11 +92,14 @@ void doGetFood(const int& whichAgent)
 	// get agent position
 	int agentPos = static_cast<int> (floor(population[whichAgent].moveDistCopy));
 
+	// if agents try to cross max land they stop at max land
+	int whichLand = (agentPos >= maxLand) ? (maxLand - 1) : agentPos;
+
 	// count agents at pos
-	int neighbours = landscape[agentPos].nAgents > 0 ? landscape[agentPos].nAgents : 1;
+	int neighbours = landscape[whichLand].nAgents > 0 ? landscape[whichLand].nAgents : 1;
 
 	// value of food shared with neighbours
-	float food = static_cast<float> (landscape[agentPos].dFood) / (static_cast<float>(neighbours));
+	float food = static_cast<float> (landscape[whichLand].dFood) / (static_cast<float>(neighbours));
 
 	// energy in if move is true - loop following is penalised
 	agentEnergyVec[whichAgent] += (population[whichAgent].move) ? food : 0.000001;
