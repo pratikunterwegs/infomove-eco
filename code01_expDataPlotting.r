82915cd9cf28c47429e6d55c59a79e9f7cfcc3aa
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-25 18:14:39 +0200

    better plotting

--- code01_expDataPlotting.r
+++ code01_expDataPlotting.r
@@ -12,7 +12,7 @@ library(scico)
 ci = function(x){qnorm(0.975)*sd(x, na.rm = T)/sqrt(length(x))}
 
 # load datas
-data = fread("migSimCode/dataOutExp04.csv")
+data = fread("migSimCode/dataOutExp05.csv")
 
 # make numeric
 data = data[,lapply(.SD, as.numeric), .SDcols = c(names(data))]
@@ -22,11 +22,11 @@ dataSub = data#[gen %% 5 == 0,]
 #### make plots ####
 # plot difference from optimum of used and inherited param
 {
-  dataSum = dataSub[,.(gen,id,movep,movepcopy)
-                    ][,diff:=movepcopy-movep]
-  dataSum = melt(dataSum, id.vars = c("gen","id"))
-  dataSum = dataSum[,`:=`(roundval = plyr::round_any(value, 20))
-                  ][,.N, by = c("gen", "roundval","variable")]
+  dataSum = dataSub[time %% 5 == 0,.(gen,id,movep,movepcopy,time)
+                    ]
+  dataSum = melt(dataSum, id.vars = c("gen","id","time"))
+  dataSum = dataSum[,`:=`(roundval = plyr::round_any(value, 5))
+                  ][,.N, by = c("gen","time", "roundval","variable")]
 }
 
 # labels
@@ -34,30 +34,32 @@ labels = c(movep = "inherited", movepcopy = "used", diff = "difference")
 
 # plot fig
 ggplot(dataSum)+
-  geom_tile(aes(x = gen, y = roundval, fill = N))+
-  facet_wrap(~variable, labeller = labeller(variable = labels))+
-  scale_fill_scico(palette = "lapaz", direction = 1)+
-  theme_gdocs()+
-  ylim(-10, 1000)+
+  geom_tile(aes(x = (gen), y = roundval, fill = N))+
+  facet_grid(time~variable, labeller = labeller(variable = labels))+
+  scale_fill_scico()+
+  # scale_x_continuous(breaks = seq(0, 1e3, 25))+
+  theme_void()+
+  ylim(-10, 100)+
   labs(x = "generation", y = "movement distance",
-       title = "depletable land, N agents = 500, genMax = 200, tMax = 10",
-       subtitle = "init move = 0.0, init follow = -1.0 : 1.0")
+       title = "depletable land, N agents = 500, genMax = 1000, tMax = 25",
+       subtitle = "init move = 0.0, init follow = -10.0 : 10.0")
+
 
 # export plot
-ggsave(filename = "figs/figExploit.png", device = png(), width = 10, 
+ggsave(filename = "figs/figGenExploit.png", device = png(), width = 10, 
        height = 8, dpi = 300); dev.off()
 
 #### plot of leadership behaviour ####
 {
   dataLead = dataSub[,.(gen,id,movep,movepcopy)
                      ][,bfollow:=(movepcopy-movep)!=0]
-  dataLead = dataLead[,.(prop = .N/5e3), by = c("gen", "bfollow")]
+  dataLead = dataLead[,.(prop = .N), by = c("gen", "bfollow")]
 }
 
 # plot fig
 ggplot(dataLead)+
   geom_tile(aes(x = gen, y = bfollow, fill = prop))+
-  scico::scale_fill_scico(direction = -1, palette = "oleron")+
+  scico::scale_fill_scico(direction = 1)+
   scale_y_discrete(labels = c("indep.", "follow"))+
   ggthemes::theme_clean()+
   labs(x = "generation", y = "strategy",
@@ -65,15 +67,15 @@ ggplot(dataLead)+
        subtitle = "init move = 0.0 : 100.0, init follow = -1.0 : 1.0")
 
 # export plot
-ggsave(filename = "figs/figFollowPropExp.png", device = png(), width = 10, height = 4, dpi = 300); dev.off()
+ggsave(filename = "figs/figFollowPropExpCopy.png", device = png(), width = 10, height = 4, dpi = 300); dev.off()
 
 #### chain length over time ####
 # prep data
 {
-  dataChain = data[,.(prop = .N/5e3), by = c("gen", "chainlength")]
+  dataChain = data[,.(prop = .N), by = c("gen", "chainlength")]
 }
 
 ggplot(dataChain)+
   geom_tile(aes(x = gen, y = chainlength, fill = prop))+
-  scico::scale_fill_scico(palette = "lapaz", direction = -1)+
+  scico::scale_fill_scico(direction = 1)+
   ggthemes::theme_clean()
