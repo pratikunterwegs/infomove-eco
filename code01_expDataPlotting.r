1a7380844c643707166b84cac30e952114e1d036
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-26 21:26:20 +0200

    new plot files

--- code01_expDataPlotting.r
+++ code01_expDataPlotting.r
@@ -12,7 +12,7 @@ library(scico)
 ci = function(x){qnorm(0.975)*sd(x, na.rm = T)/sqrt(length(x))}
 
 # load datas
-data = fread("migSimCode/dataOutExp05.csv")
+data = fread("migSimCode/dataOutExp06.csv")
 
 # make numeric
 data = data[,lapply(.SD, as.numeric), .SDcols = c(names(data))]
@@ -22,11 +22,11 @@ dataSub = data#[gen %% 5 == 0,]
 #### make plots ####
 # plot difference from optimum of used and inherited param
 {
-  dataSum = dataSub[time %% 5 == 0,.(gen,id,movep,movepcopy,time)
+  dataSum = dataSub[gen %% 100 == 0,.(gen,id,movep,movepcopy)
                     ]
-  dataSum = melt(dataSum, id.vars = c("gen","id","time"))
-  dataSum = dataSum[,`:=`(roundval = plyr::round_any(value, 5))
-                  ][,.N, by = c("gen","time", "roundval","variable")]
+  dataSum = melt(dataSum, id.vars = c("gen","id"))
+  #dataSum = dataSum[,`:=`(roundval = plyr::round_any(value, 5))
+   #               ][,.N, by = c("gen", "roundval","variable")]
 }
 
 # labels
@@ -34,15 +34,19 @@ labels = c(movep = "inherited", movepcopy = "used", diff = "difference")
 
 # plot fig
 ggplot(dataSum)+
-  geom_tile(aes(x = (gen), y = roundval, fill = N))+
-  facet_grid(time~variable, labeller = labeller(variable = labels))+
-  scale_fill_scico()+
+  geom_density(aes(x = value, fill = variable), alpha=0.6,
+               col = "transparent")+
+  facet_grid(~gen, 
+             labeller = labeller(variable = labels),
+             scales = "free_y")+
+  scale_fill_scico_d(palette = "roma")+
   # scale_x_continuous(breaks = seq(0, 1e3, 25))+
-  theme_void()+
-  ylim(-10, 100)+
+  theme_clean()+
+  xlim(-10, 40)+
+  coord_cartesian(ylim=c(0, 0.2))+
   labs(x = "generation", y = "movement distance",
-       title = "depletable land, N agents = 500, genMax = 1000, tMax = 25",
-       subtitle = "init move = 0.0, init follow = -10.0 : 10.0")
+       title = "depletable land, N agents = 100, genMax = 1000, tMax = 25",
+       subtitle = "init move = 0.0, init follow = 0.0")
 
 
 # export plot
@@ -53,7 +57,7 @@ ggsave(filename = "figs/figGenExploit.png", device = png(), width = 10,
 {
   dataLead = dataSub[,.(gen,id,movep,movepcopy)
                      ][,bfollow:=(movepcopy-movep)!=0]
-  dataLead = dataLead[,.(prop = .N), by = c("gen", "bfollow")]
+  dataLead = dataLead[,.(prop = .N/2500), by = c("gen", "bfollow")]
 }
 
 # plot fig
@@ -72,10 +76,11 @@ ggsave(filename = "figs/figFollowPropExpCopy.png", device = png(), width = 10, h
 #### chain length over time ####
 # prep data
 {
-  dataChain = data[,.(prop = .N), by = c("gen", "chainlength")]
+  dataChain = data[,.(prop = .N), by = c("gen", "time", "chainlength")]
 }
 
 ggplot(dataChain)+
   geom_tile(aes(x = gen, y = chainlength, fill = prop))+
   scico::scale_fill_scico(direction = 1)+
+  facet_wrap(~time)+
   ggthemes::theme_clean()
