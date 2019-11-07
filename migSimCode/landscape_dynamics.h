28bcaaa913e19f9ebc3aa0bd71e2687867492f69
pratikunterwegs <pratikgupte16@gmail.com>
2019-11-07 18:02:57 +0100

    move functions to agents

--- migSimCode/landscape_dynamics.h
+++ migSimCode/landscape_dynamics.h
@@ -4,7 +4,7 @@
 // to be imported after agents.h
 
 #include "params.h"
-#include "agents.h"
+
 // make gridcell class
 class gridcell
 {
@@ -34,51 +34,6 @@ float getWrappedDist(const float& x1, const float& x2)
     assert(tempdist <= maxLandPos && "tempdist is beyond land");
     tempdist = std::min(tempdist, maxLandPos - tempdist);
     return tempdist;
-}
-
-/// function to deplete landscape
-// update dFood based on wrapped agent effect
-void depleteFood(const int& whichAgent)
-{
-    assert(population[whichAgent].circPos >= 0 && "pop has neg moves");
-    for(int l = 0; l < landPoints; l++)
-    {
-        // wrapped distance from agent
-        float dist = getWrappedDist(population[whichAgent].circPos, landscape[l].dPos);
-		float depleted = (maxDepletion / (1.f + exp(depletionSlope * (dist - depletionRadius))));
-
-		landscape[l].dFood -= (landscape[l].dFood - depleted) > 0.f ? depleted : landscape[l].dFood;
-
-		assert(landscape[l].dFood >= 0.f && "landscape food has become negative!");
-    }    
-}
-
-/// function to get energy
-void doGetFood(const int& whichAgent)
-{
-	// loop through landscape looking for pair of positions
-    int l = 0;
-    int bound_right = 0;
-    while((l < landPoints))
-    {
-        // get right bound
-        bound_right = landscape[bound_right].dPos > population[whichAgent].circPos ? bound_right : l;
-        l++;
-    }
-
-    // left bound is right bound - 1
-    int bound_left = (bound_right - 1 >= 0)? (bound_right - 1): landPoints + (bound_right - 1);
-    // energy is left bound / left distance + right bound / right distance
-    float dist_left = getWrappedDist(population[whichAgent].circPos, landscape[bound_left].dPos);
-    float dist_right = getWrappedDist(population[whichAgent].circPos, landscape[bound_right].dPos);
-
-    float food_left = landscape[bound_left].dFood;
-    float food_right = landscape[bound_right].dFood;
-    // agent foraging is interpolated
-    agentEnergyVec[whichAgent] += ((food_left * dist_left) + (food_right * dist_right)) / (dist_left + dist_right);
-
-    // std::cout << "agent " << whichAgent << " energy = " << agentEnergyVec[whichAgent] << "\n";
-    
 }
 
 /// function to replenish food each generations
