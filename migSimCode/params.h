2fdb90e3ee4ff019ca2a41d0460d29e58e0427a1
pratikunterwegs <pratikgupte16@gmail.com>
2019-11-08 17:30:04 +0100

    increase max food

--- migSimCode/params.h
+++ migSimCode/params.h
@@ -25,7 +25,7 @@ const int popsize = 100; // not too many
 // landscape params
 const int landPoints = 10;
 const float maxLandPos = 20.f;
-const float maxFood = 1.f;
+const float maxFood = 10.f;
 const float maxDepletion = 0.01f;
 const float depletionSlope = 5.f;
 const float depletionRadius = 2.f;
