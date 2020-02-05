e4afee79a602aaedd254de315387f48e21a43436
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-05 17:57:58 +0100

    low popsize more foraging events

--- migSimCode/params.h
+++ migSimCode/params.h
@@ -20,21 +20,22 @@
 //}
 
 // population params
-const int popsize = 1000; // not too many
+const int popsize = 100; // not too many
 
 // agent params
 const float moveDist = 0.01f;
 
 // landscape params
-const int landPoints = 100;
+const int n_patches = 10;
 const float maxFood = 10.f;
 const float maxDepletion = 0.01f;
-const float deadZone = 0.001f * 1.0f; // 0.1 % of the landscape size is a deadzone around each agent
-const float deplZone = 0.005f * 1.0f; // 0.5 % of the landscape size on each side of the agent is a deadzone
+//const float deadZone = 0.001f * 1.0f; // 0.1 % of the landscape size is a deadzone around each agent
+//const float deplZone = 0.005f * 1.0f; // 0.5 % of the landscape size on each side of the agent is a deadzone
 
 // time params
-const int genmax = 1000; //arbit choice
-const int tMax = 25;
+const int genmax = 100; //arbit choice
+const int tMax = 100;
+const int foraging_turns = 10;
 
 // rand number
 std::random_device rd;
