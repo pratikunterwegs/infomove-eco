b3d3ee1376f3f1c1a9a8d3086b61edb0b9d664bb
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-20 16:10:03 +0100

    smplify foraging on landscape

--- migSimCode/landscape_dynamics.hpp
+++ migSimCode/landscape_dynamics.hpp
@@ -31,76 +31,38 @@ int wrapper(int distance, int current_val, int max_val) {
 	return new_pos;
 }
 
-void agent::goToLandscape(landscape& landscape)
-{
-    landscape.foragers[static_cast<size_t>(pos)] += 1;
-}
-
 /// function to walk along the circle
-void agent::circleWalk(landscape& landscape)
+void agent::circleWalk()
 {
 	// check where agent is
 	assert(pos <= n_patches - 1 && "func circleWalk: agent now over max land!");
 	assert(pos >= 0 && "func circleWalk: agent now over min land!");
 
-	// move up the peak
-	int pos_left = wrapper(-1, pos, n_patches);
-	int pos_right = wrapper(1, pos, n_patches);
+    pos = wrapper(tradeOffParam, pos, n_patches);
 
-	// remove agent from previous cell
-    landscape.foragers[static_cast<size_t>(pos)] -= 1;
-    assert(landscape.foragers[static_cast<size_t>(pos)] >= 0
-            && "func circleWalk: cell has neg agents!");
-
-    pos = (landscape.resources[static_cast<size_t>(pos)] >
-            landscape.resources[static_cast<size_t>(pos)]) ? pos_left : pos_right;
-
-    landscape.foragers[static_cast<size_t>(pos)] += 1;
+    //landscape.foragers[static_cast<size_t>(pos)] += 1;
 
 	assert(pos <= n_patches - 1 && "func circleWalk: agent walked over max land!");
 	assert(pos >= 0 && "func circleWalk: agent walked over min land!");
 
 }
 
-/// function to choose explore or exploit
-void agent::exploreOrExploit(landscape& landscape)
-{
-    if (tradeOffParam <= tradeoff_picker(rng))
-	{
-        circleWalk(landscape);
-		total_distance += 1;
-		// deplete energy
-		energy -= move_cost;
-		// energy = energy >= 0.000001f ? energy : 0.000001f;
-	}
-	else
-	{
-        depleteFood(landscape);
-	}
-
-	// remember food at current pos
-    mem_last_pos = landscape.resources[static_cast<size_t>(pos)];
-}
-
-
 /// function to implement the foraging dynamic with turns and tradeoff
 void do_foraging_dynamic(landscape& landscape, std::vector<agent>& population, int turns)
 {
-	// all agents go to landscape
-	for (size_t indiv = 0; indiv < popsize; indiv++)
-	{
-        population[indiv].goToLandscape(landscape);
-	}
-
     for (size_t turn_value = 0; static_cast<int>(turn_value) < turns; turn_value++)
-	{
-		// over as many iterations as foraging_turns agents choose
-		// to explore or exploit
-		for (size_t indiv = 0; indiv < popsize; indiv++)
-		{
-            population[indiv].exploreOrExploit(landscape);
-		}
-	}
+    {
+        // over as many iterations as foraging_turns agents choose
+        // to explore or exploit
+        for (size_t indiv = 0; indiv < popsize; indiv++)
+        {
+            population[indiv].circleWalk();
+            population[indiv].depleteFood(landscape);
+
+            // remember food at current pos
+            population[indiv].mem_last_pos = landscape.resources[static_cast<size_t>(population[indiv].pos)];
+        }
+    }
 }
 
 
