9036e4a40146b3e4425eeebe90ff97f78298b3d8
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-20 09:31:47 +0100

    add regime shift func

--- migSimCode/simulations.hpp
+++ migSimCode/simulations.hpp
@@ -15,11 +15,11 @@
 
 /// function to evolve population
 std::vector<agent> evolve_pop(const int genmax, const int timesteps,
-                              const int foraging_turns, float R)
+                              const int foraging_turns, float R,
+                              landscape& landscape)
 {
     //Initialization of pop and landscape
     std::vector<agent> pop;
-    landscape landscape;
     R = powf(10.f, R);
 
     // run over n agents
@@ -46,3 +46,39 @@ std::vector<agent> evolve_pop(const int genmax, const int timesteps,
     std::cout << "pop evolved...\n";
     return pop;
 }
+
+/// function to change regime
+/// function to evolve population
+std::vector<agent> regime_shift(std::vector<agent> pop, landscape& landscape,
+                                const int genmax, const int timesteps,
+                              const int foraging_turns, float new_R)
+{
+    //select a new R
+    new_R = powf(10.f, new_R);
+
+    // run over n agents
+    for (int gen; gen < genmax; gen++) {
+        std::cout << "gen = " << gen << "\n";
+        // loop through timesteps
+        for (int t = 0; t < timesteps; t++) {
+            // shuffle population
+            shufflePopSeq(pop);
+            // reset leaders
+            for (size_t ind = 0; static_cast<int>(ind) < popsize; ind++) {
+                doFollowDynamic(pop);
+                do_foraging_dynamic(landscape, pop, foraging_turns);
+            }
+        }
+        float regrowth = std::sin((2.f * static_cast<float>(M_PIl) *
+                                    static_cast<float>(static_cast<float>(gen))) /
+                                   (new_R));
+        landscape.doMakeFood(regrowth);
+        do_reprod(pop);
+
+    }
+
+    std::cout << "regime shifted...\n";
+    return pop;
+}
+
+// ends here
