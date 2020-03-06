ddf09aca3bd95b9c754f2c7e0b52e5a08f1d29c7
Pratik Gupte <pratikgupte16@gmail.com>
2020-03-06 17:28:58 +0100

    remove summary output

--- migSimCode/utilities.hpp
+++ migSimCode/utilities.hpp
@@ -9,7 +9,7 @@ void printAgents(std::vector<agent> population, const int& gen_p, const int& tim
 	agentofs.open("dataAgents.csv", std::ofstream::out | std::ofstream::app);
 
 	// col header
-	if (gen_p == 0 && time_p == 0) { agentofs << "gen,time,id,eeParam,pos,leader,energy\n"; }
+    if (gen_p == 0 && time_p == 0) { agentofs << "gen,time,D,M,pos,energy\n"; }
 
 	// print for each ind
 	if ((gen_p == 0 || gen_p % 1 == 0) && time_p % 5 == 0)
@@ -19,37 +19,9 @@ void printAgents(std::vector<agent> population, const int& gen_p, const int& tim
 			agentofs
 				<< gen_p << ","
 				<< time_p << ","
-				<< population[ind2].id_self << ","
-				<< population[ind2].tradeOffParam << ","
-				<< population[ind2].pos << ","
-				<< population[ind2].id_leader << ","
-				<< population[ind2].energy << "\n";
-		}
-	}
-	// close
-	agentofs.close();
-}
-
-/// print agent summary after gen
-void print_agent_summary(std::vector<agent> population, const int& gen_p)
-{
-	// open or append
-	std::ofstream agentofs;
-	agentofs.open("data_agent_summary.csv", std::ofstream::out | std::ofstream::app);
-
-	// col header
-	if (gen_p == 0) { agentofs << "gen,tradeoff,distance,prop_indep,energy\n"; }
-
-	// print for each ind
-	if ((gen_p == 0 || gen_p % 1 == 0))
-	{
-        for (size_t ind2 = 0; static_cast<int>(ind2) < popsize; ind2++)
-		{
-			agentofs
-				<< gen_p << ","
-				<< population[ind2].tradeOffParam << ","
-				<< population[ind2].total_distance << ","
-				<< static_cast<float>(population[ind2].follow_instances) / static_cast<float>(tMax) << ","
+                << population[ind2].D << ","
+                << population[ind2].M << ","
+                << population[ind2].pos << ","
 				<< population[ind2].energy << "\n";
 		}
 	}
