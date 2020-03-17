1557e45ba101686f0712667186b83727e0dbe1c1
pratikunterwegs <pratikgupte16@gmail.com>
2020-03-17 10:22:41 +0100

    add basic test

--- migSimCode/source.cpp
+++ migSimCode/source.cpp
@@ -15,10 +15,8 @@
 #include "simulations.hpp"
 #include "utilities.hpp"
 
-// tests section
-
-/// the main function
-int main()
+/// evolution dynamics function
+int do_main()
 {
     // init new landscape
     landscape landscape;
@@ -30,8 +28,30 @@ int main()
     evolved_pop = evolve_pop(evolved_pop, genmax, tMax, foraging_turns,
                                                 RHO, PHI, landscape);
 
-	std::cout << "works so far\n";
-	return 0;
+    std::cout << "works so far\n";
+    return 0;
+}
+
+// tests section
+/// function to test program
+void basic_test()
+{
+    landscape test_landscape;
+    std::vector<agent> test_pop(10);
+    test_pop = evolve_pop(test_pop, 10, 10, 10, 1.f, 1.f, test_landscape);
+
+    // check basic output
+    assert(static_cast<int>(test_pop.size()) == 10);
+    assert(do_main() == 0);
+}
+
+/// main function
+int main()
+{
+    // ensure tests run
+    basic_test();
+    // print output
+    return do_main();
 }
 
 // end here
