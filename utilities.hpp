c6e42a610c785bebd378ac037b65043bfce78fb3
pratikunterwegs <pratikgupte16@gmail.com>
2020-03-23 13:35:06 +0100

    add params to print for data

--- utilities.hpp
+++ utilities.hpp
@@ -1,55 +1,29 @@
+#ifndef UTILITIES
+#define UTILITIES
+
 #include "agents.hpp"
 
-/// function to print data
-// func must print gen, id, distance from peak, move param, leader, energy at the end of each generation
-void printAgents(std::vector<agent> population, const int& gen_p, const int& time_p)
+/// function to print data from an evolved population
+void print_agents(std::string output, std::vector<agent> &pop)
 {
 	// open or append
 	std::ofstream agentofs;
-	agentofs.open("dataAgents.csv", std::ofstream::out | std::ofstream::app);
+	agentofs.open(output, std::ofstream::out);
 
 	// col header
-    if (gen_p == 0 && time_p == 0) { agentofs << "gen,time,D,M,pos,energy\n"; }
-
-	// print for each ind
-	if ((gen_p == 0 || gen_p % 1 == 0) && time_p % 5 == 0)
+	// D = giving up density, M = exploration range
+	// a,b,c = evolved params for agent assessment
+	agentofs << "D,M,a,b,c,pos,energy\n"; }
+	size_t p_out = static_cast<size_t>(popsize);
+	for (size_t i_out = 0; i_out < p_out; i_out++)
 	{
-        for (size_t ind2 = 0; static_cast<int>(ind2) < popsize; ind2++)
-        {
-			agentofs
-				<< gen_p << ","
-				<< time_p << ","
-                << population[ind2].D << ","
-                << population[ind2].M << ","
-                << population[ind2].pos << ","
-				<< population[ind2].energy << "\n";
-		}
+		agentofs << pop[i_out].D << ","
+			<< pop[i_out].M << ","
+			<< pop[i_out].a << "," << pop[i_out].b << "," << pop[i_out].c << ","
+			<< pop[i_out].pos << ","
+			<< pop[i_out].energy << "\n";
 	}
-	// close
 	agentofs.close();
 }
 
-/// function to print landscape values
-//void printLand(const int& gen_p, const int& t_p)
-//{
-//	// open or append
-//	std::ofstream landofs;
-//	landofs.open("dataLand.csv", std::ofstream::out | std::ofstream::app);
-//	// col header
-//	if ((gen_p == 0) && (t_p == 0)) { landofs << "gen,t,pos,food\n"; }
-//	// print for each land cell
-//	{
-//		for (int landcell = 0; landcell < landscape.size(); landcell++)
-//		{
-//			landofs
-//				<< gen_p << ","
-//				<< t_p << ","
-//				<< landcell << ","
-//				<< landscape[landcell].dFood << "\n";
-//		}
-//		//close
-//		landofs.close();
-//	}
-//}
-
-// ends here
+#endif /* end of include guard: UTILITIES */
