06fdd1538acf1b0b387613766ab0e1e3ff726839
pratikunterwegs <pratikgupte16@gmail.com>
2020-03-23 13:49:22 +0100

    fix printing func

--- utilities.hpp
+++ utilities.hpp
@@ -2,9 +2,10 @@
 #define UTILITIES
 
 #include "agents.hpp"
+#include <vector>
 
 /// function to print data from an evolved population
-void print_agents(std::string output, std::vector<agent> &pop)
+void print_agents(std::string output, std::vector<agent> pop)
 {
 	// open or append
 	std::ofstream agentofs;
@@ -13,16 +14,15 @@ void print_agents(std::string output, std::vector<agent> &pop)
 	// col header
 	// D = giving up density, M = exploration range
 	// a,b,c = evolved params for agent assessment
-	agentofs << "D,M,a,b,c,F,pos,energy\n"; }
-	size_t p_out = static_cast<size_t>(popsize);
-	for (size_t i_out = 0; i_out < p_out; i_out++)
+    agentofs << "D,M,a,b,c,F,pos,energy\n";
+    for (size_t iout = 0; iout < pop.size(); iout++)
 	{
-		agentofs << pop[i_out].D << ","
-			<< pop[i_out].M << ","
-			<< pop[i_out].a << "," << pop[i_out].b << "," << pop[i_out].c << ","
-			<< pop[i_out].prop_follow << ","
-			<< pop[i_out].pos << ","
-			<< pop[i_out].energy << "\n";
+		agentofs << pop[iout].D << ","
+			<< pop[iout].M << ","
+			<< pop[iout].a << "," << pop[iout].b << "," << pop[iout].c << ","
+			<< pop[iout].prop_follow << ","
+			<< pop[iout].pos << ","
+			<< pop[iout].energy << "\n";
 	}
 	agentofs.close();
 }
