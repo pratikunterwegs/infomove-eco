0032a855f80f6fd2e6d26a943e35993a5b955b68
pratikunterwegs <pratikgupte16@gmail.com>
2020-03-23 13:40:37 +0100

    get prop_follow and print

--- utilities.hpp
+++ utilities.hpp
@@ -13,13 +13,14 @@ void print_agents(std::string output, std::vector<agent> &pop)
 	// col header
 	// D = giving up density, M = exploration range
 	// a,b,c = evolved params for agent assessment
-	agentofs << "D,M,a,b,c,pos,energy\n"; }
+	agentofs << "D,M,a,b,c,F,pos,energy\n"; }
 	size_t p_out = static_cast<size_t>(popsize);
 	for (size_t i_out = 0; i_out < p_out; i_out++)
 	{
 		agentofs << pop[i_out].D << ","
 			<< pop[i_out].M << ","
 			<< pop[i_out].a << "," << pop[i_out].b << "," << pop[i_out].c << ","
+			<< pop[i_out].prop_follow << ","
 			<< pop[i_out].pos << ","
 			<< pop[i_out].energy << "\n";
 	}
