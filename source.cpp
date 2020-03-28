891c66d5d89cfd2d67498e4bbd9be0cb6d5f23f6
pratikunterwegs <pratikgupte16@gmail.com>
2020-03-29 00:37:29 +0100

    cli args

--- source.cpp
+++ source.cpp
@@ -15,39 +15,66 @@
 #include "simulations.hpp"
 #include "utilities.hpp"
 
-/// evolution dynamics function
-int do_main()
+// test wraparound
+void test_wraparound()
 {
-    // init new landscape
-    landscape landscape;
-    std::vector<agent> evolved_pop (popsize);
+    int wrapped_val = wraparound(120, 100, 0);
+    assert(wrapped_val == 20);
 
-    float RHO = 1.f;
-    float PHI = 1.f;
-
-    evolved_pop = evolve_pop(evolved_pop, genmax, tMax, foraging_turns,
-                                                RHO, PHI, landscape);
-
-    print_agents("data/data_agents.csv", evolved_pop);
-
-    std::cout << "works so far\n";
-    return 0;
+    wrapped_val = wraparound(-20, 100, 0);
+    assert(wrapped_val == 80);
 }
 
-// tests section
-/// function to test program
-void basic_test()
+
+// test command line args
+void test_cli_args(std::vector<std::string> cli_args) noexcept
 {
-    assert(do_main() == 0);
+    // check for size
+    if(cli_args.size() != 6) // count RHO PHI gens timesteps turns
+    {
+        std::cerr << "wrong number of command args\n";
+    }
+
+    // check args content
+    for (size_t arg = 1; arg < 3; arg++) {
+        assert(std::stof(cli_args[arg]) > 0.f && "cli_args: negative!");
+    }
+    for (size_t arg = 3; arg < 5; arg++) {
+        assert(std::stoi(cli_args[arg]) > 0.f && "cli_args: negative!");
+    }
 }
 
 /// main function
-int main()
+int main(int argc, char* argv[])
 {
-    // ensure tests run
-    basic_test();
-    // print output
-    return do_main();
+    //basic tests
+    test_wraparound();
+    std::vector<std::string> cli_args(argv, argv+argc);
+    test_cli_args(cli_args);
+
+    // init gsl rng
+    const gsl_rng_type * T;
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc (T);
+
+    // gather cli args
+    const float RHO = std::stof(cli_args[1]);
+    const float PHI = std::stof(cli_args[2]);
+    const int genmax = std::stoi(cli_args[3]);
+    const int timesteps = std::stoi(cli_args[4]);
+    const int turns = std::stoi(cli_args[5]);
+
+    // init pop and landscape
+    std::vector<agent> pop (popsize);
+    landscape landscape_;
+
+    // do simulation
+    evolve_pop(pop, genmax, timesteps, turns, RHO, PHI, landscape_);
+
+    // print evolved population params
+
+    // print landscape as well
 }
 
 // end here
