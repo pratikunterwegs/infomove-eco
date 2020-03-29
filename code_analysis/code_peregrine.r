93b8fe0eb454f4ac2bc9912b56e1cef891e62fab
pratikunterwegs <pratikgupte16@gmail.com>
2020-03-29 15:36:18 +0200

    placing job output on cluster

--- code_analysis/code_peregrine.r
+++ code_analysis/code_peregrine.r
@@ -7,13 +7,14 @@ library(glue)
 
 # make dataframe of strategy, m_param, and t_max
 # these will be passed as cli args
-{rho = as.double(seq(0, 5, 0.5))
-phi = as.double(1.0)
-gens = 1000000
+{rho = as.character(seq(0.5, 5, 0.5))
+phi = as.character(1.0)
+gens = 100000
 timesteps = 100
-turns = 5}
+turns = 5
+rep = 1:10}
 
-sim_params = crossing(rho, phi, gens, timesteps, turns)
+sim_params = crossing(rho, phi, gens, timesteps, turns, rep)
 rm(rho, phi, gens, timesteps, turns)
 
 # read peregrine password
@@ -26,29 +27,33 @@ ssh_exec_wait(s, command = c("cd infomove/",
                              "rm *.sh",
                              "cd ..",
                              "git pull",
+                             "ml load GCC/8.3.0",
+                             "ml load GSL/2.6-GCC-8.3.0",
+                             "qmake infomove.pro",
                              "make --silent"))
 
 # send commands
 shebang <- readLines("code_analysis/template_job.sh")
-pwalk(sim_params, function(rho, phi, gens, timesteps, turns){
+pwalk(sim_params, function(rho, phi, gens, timesteps, turns, rep){
   
   if(!dir.exists("jobs")){
     dir.create("jobs")
   }
   
-  shebang[2] <- glue('#SBATCH --job-name=run_infomove_phi{phi}_rho{rho}_time{timesteps}_turns{turns}')
+  shebang[2] <- glue('#SBATCH --job-name=run_infomove_phi{phi}_rho{rho}_time{timesteps}_turns{turns}_rep{rep}')
   {
-    command <- glue('./infomove {phi} {rho} {gens} {timesteps} {turns}')
-    jobfile <- glue('jobs/job_infomove_phi{phi}_rho{rho}_time{timesteps}_turns{turns}.sh')
+    command <- glue('.././infomove {phi} {rho} {gens} {timesteps} {turns} {rep}')
+    jobfile <- glue('job_infomove_phi{phi}_rho{rho}_time{timesteps}_turns{turns}_rep{rep}.sh')
     
-    writeLines(c(shebang, command), con = jobfile)
-    scp_upload(s, jobfile, to = "infomove/jobs/")
-    file.remove(jobfile)
+    writeLines(c(shebang, command), con = glue('jobs/{jobfile}'))
+    scp_upload(s, glue('jobs/{jobfile}'), to = "infomove/jobs/")
+    file.remove(glue('jobs/{jobfile}'))
   }
   
   # run jobs; we are in infomove
-  # ssh_exec_wait(s, command = glue('dos2unix {jobfile}'))
-  # ssh_exec_wait(s, command = c(glue('sbatch {jobfile}')))
+  ssh_exec_wait(s, command = c("cd infomove/jobs",
+                               glue('dos2unix {jobfile}'),
+                               glue('sbatch {jobfile}')))
 })
 
 # move into jobs, delete scripts and disconnect
