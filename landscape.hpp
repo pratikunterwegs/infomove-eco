f3d6147f32f6a9c3d02c1558673f595bef96a340
pratikunterwegs <pratikgupte16@gmail.com>
2020-03-30 13:50:15 +0200

    change phi rho order and update replenish function

--- landscape.hpp
+++ landscape.hpp
@@ -14,23 +14,26 @@ public:
     landscape();
     std::vector<float> resources;
 
-    void doMakeFood(const float& rho, const float& phi, const float& delta);
+    void doMakeFood(const float& rho, const float& phi);
 
 };
 
 /// landscape constructor
 landscape::landscape(){
-    resources = std::vector<float> (n_patches, maxFood);
+    resources = std::vector<float> (n_patches, 0.f);
 }
 
 /// function to replenish food each generations
-void landscape::doMakeFood(const float& rho, const float& phi, const float& delta)
+void landscape::doMakeFood(const float& phi, const float& rho)
 {
     float max_land = 0.f, min_land = 0.f, range_land = 0.f;
     for (size_t l = 0; static_cast<int>(l) < n_patches; l++)
     {
-        resources[l] = sinf(static_cast<float>(M_PIl) * l * phi + delta) +
-                (rho * static_cast<float>(gsl_ran_gaussian(r, 0.1)));
+        // r = sin(2pi * phi * l / n_patches)
+        resources[l] = sinf((2.f * static_cast<float>(M_PIl) * (static_cast<float>(l) / static_cast<float>(n_patches)) * phi)) +
+                (static_cast<float>(gsl_ran_gaussian(r, rho)));
+
+        // normalise for negative values
         max_land = max_land > resources[l] ? max_land : resources[l];
         min_land = min_land < resources[l] ? min_land : resources[l];
         range_land = max_land - min_land;
