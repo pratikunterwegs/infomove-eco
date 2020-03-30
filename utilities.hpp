bf38741299e1c5f0411efe308afcd39a71024c1b
pratikunterwegs <pratikgupte16@gmail.com>
2020-03-30 17:30:12 +0200

    get outfile and mod print agents

--- utilities.hpp
+++ utilities.hpp
@@ -4,13 +4,11 @@
 #include "agents.hpp"
 #include <vector>
 
-/// function to print data from an evolved population
-void print_agents(std::vector<agent> pop,
-                  const int phi, const float rho,
-                  const int timesteps,
-                  const float init_d,
-                  const int rep){
-
+/// construct agent output filename
+std::string identify_outfile(const int phi, const float rho,
+                        const int timesteps,
+                        const float init_d,
+                        const int rep){
     // output filename
     const std::string outfile = "data/agent_" +
             std::to_string(phi) + "_" +
@@ -20,18 +18,6 @@ void print_agents(std::vector<agent> pop,
             "_rep_" + std::to_string(rep) +
             ".csv";
 
-    std::ofstream agent_ofs;
-    // append data to existing ofs
-    agent_ofs.open(outfile, std::ofstream::out);
-    agent_ofs << "id,D,M,a,b,F\n";
-    for (size_t i = 0; i < pop.size(); i++) {
-        agent_ofs << i << ","
-                 << pop[i].D << ","
-                 << pop[i].M << ","
-                 << pop[i].a << ","
-                 << pop[i].b << ","
-                 << pop[i].prop_follow << "\n";
-    }
 
     // write summary with filename to agent data
     // and parameter files
@@ -44,15 +30,49 @@ void print_agents(std::vector<agent> pop,
     if(!f2.good()){
         summary_ofs.open(summary_out, std::ofstream::out);
         summary_ofs << "filename,phi,rho,timesteps,init_d,rep\n";
+        summary_ofs.close();
     }
     // append if not
-    summary_ofs.open(summary_out, std::ofstream::app);
+    summary_ofs.open(summary_out, std::ofstream::out | std::ofstream::app);
     summary_ofs << outfile << ","
                 << phi << ","
                 << rho << ","
                 << timesteps << ","
                 << init_d << ","
                 << rep << "\n";
+    summary_ofs.close();
+
+    return outfile;
+}
+
+/// function to print data from an evolved population
+std::string print_agents(std::vector<agent> pop,
+                  const int gen, std::string outfile){
+
+    std::ofstream agent_ofs;
+    // append data to existing ofs
+
+    // check if file is okay
+    std::ifstream f(outfile);
+    if(!f.good()){
+        agent_ofs.open(outfile, std::ofstream::out);
+        agent_ofs << "gen,id,D,M,a,b,F\n";
+        agent_ofs.close();
+    }
+
+    for (size_t i = 0; i < pop.size(); i++) {
+        agent_ofs.open(outfile, std::ofstream::out | std::ofstream::app);
+        agent_ofs << gen << ","
+                  << i << ","
+                  << pop[i].D << ","
+                  << pop[i].M << ","
+                  << pop[i].a << ","
+                  << pop[i].b << ","
+                  << pop[i].prop_follow << "\n";
+        agent_ofs.close();
+    }
+
+    return outfile;
 }
 
 #endif /* end of include guard: UTILITIES */
