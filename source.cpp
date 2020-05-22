98a271c742255620bf9304b524f0314c7eaffdf9
pratik <pratikgupte16@gmail.com>
2020-05-23 01:05:58 +0200

    add more cliargs

--- source.cpp
+++ source.cpp
@@ -31,7 +31,7 @@ void test_wrap_distance()
 void test_cli_args(std::vector<std::string> cli_args) noexcept
 {
     // check for size
-    if(cli_args.size() != 9) // count type RHO PHI gens timesteps D leader choices rep
+    if(cli_args.size() != 10) // count type RHO PHI gens timesteps D leader choices rep
     {
         std::cerr << "wrong number of command args\n";
     }
