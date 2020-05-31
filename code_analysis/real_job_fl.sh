6b09e06ef995d87b4ab826f773755407dd06a2dd
pratik <pratikgupte16@gmail.com>
2020-06-01 00:27:34 +0200

    reduce header

--- code_analysis/real_job_fl.sh
+++ code_analysis/real_job_fl.sh
@@ -10,13 +10,6 @@ cd ..
 
 module load R/3.6.1-foss-2018a
 
-module load GCC/8.3.0
-module load GSL/2.6-GCC-8.3.0
-
-qmake infomove.pro
-make clean -j4
-make -j4
-
 ml list
 
 Rscript --slave -e 'infomover::run_infomove_fl(type = c("info"), phi = c(5, 25), rho = 0.1, timesteps = 100, a_res = seq(-3, 3, 0.5), b_res = seq(-3, 3, 0.5), M_res = seq(0, 8, 2), leader_choices = c(1,2,5), gradient = 0.5, gradient_m = 2, n_rep = 5)'
