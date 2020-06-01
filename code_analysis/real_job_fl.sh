ce8b55c7a19ee4ad4c99e853c5e399777583f652
pratik <pratikgupte16@gmail.com>
2020-06-01 11:55:45 +0200

    reduce combinations

--- code_analysis/real_job_fl.sh
+++ code_analysis/real_job_fl.sh
@@ -12,4 +12,4 @@ module load R/3.6.1-foss-2018a
 
 ml list
 
-Rscript --slave -e 'infomover::run_infomove_fl(type = c("info"), phi = c(5, 25), rho = 0.1, timesteps = 100, a_res = seq(-3, 3, 0.5), b_res = seq(-3, 3, 0.5), M_res = seq(0, 8, 2), leader_choices = c(1,2,5), gradient = 0.5, gradient_m = 2, n_rep = 5)'
+Rscript --slave -e 'infomover::run_infomove_fl(type = c("info"), phi = c(5, 25), rho = 0.1, timesteps = 100, a_res = seq(-3, 3, 1), b_res = seq(-3, 3, 1), M_res = seq(2, 6, 2), leader_choices = c(1,2,5), gradient = 0.5, gradient_m = 2, n_rep = 5)'
